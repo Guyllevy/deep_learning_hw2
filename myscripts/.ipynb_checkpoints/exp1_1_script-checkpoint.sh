1da0d47fa4db895677f32767f0bec9d289287c7b
guy <guylleviii@gmail.com>
2023-06-05 18:28:56 +0300

    work done on server

---
+++ myscripts/.ipynb_checkpoints/exp1_1_script-checkpoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+K_values=(32 64)
+L_values=(2 4 8 16)
+
+for K in "${K_values[@]}"; do
+  for L in "${L_values[@]}"; do
+    exp_name="exp1_1"
+    command="python -m hw2.experiments run-exp -n ${exp_name} -K ${K} -L ${L} -P 4 -H 100"
+    echo "Running command: ${command}"
+    eval "${command}"
+  done
+done
---
+++ myscripts/.ipynb_checkpoints/prep_sub-checkpoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python main.py prepare-submission --id 206865362 --id 318700291
